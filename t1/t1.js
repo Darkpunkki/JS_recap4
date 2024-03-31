f1b50710acf45125a191ed98b258a70c01f00424
Darkpunkki <jimi.pettila@metropolia.fi>
2024-03-31 17:12:47 +0300

    task 1 actually done this time

--- t1/t1.js
+++ t1/t1.js
@@ -30,26 +30,34 @@ let todoList = [
 
 const ul = document.querySelector('ul');
 
-todoList.forEach((todo) => {
-  // create the html content
-  const todoHTML = `<li id="item-${todo.id}"> <input type="checkbox" id="todo-${
-    todo.id
-  }" ${todo.completed ? 'checked' : ''} />
+function displayTodoList() {
+  // clear the ul before adding the new list
+  ul.innerHTML = '';
+  todoList.forEach((todo) => {
+    // create the html content
+    const todoHTML = `<li id="item-${
+      todo.id
+    }"> <input type="checkbox" id="todo-${todo.id}" ${
+      todo.completed ? 'checked' : ''
+    } />
     <label for="todo-${todo.id}">${todo.task}</label>
     <button class="delete-btn" onclick="deleteItem(${todo.id})">X</button>
     </li>`;
 
-  // add the html content to the ul
-  ul.insertAdjacentHTML('beforeend', todoHTML);
+    // add the html content to the ul
+    ul.insertAdjacentHTML('beforeend', todoHTML);
 
-  // add event listener to the checkbox and set the completed value to the opposite of the current value
-  document.getElementById(`todo-${todo.id}`).addEventListener('change', () => {
-    todo.completed = !todo.completed;
+    // add event listener to the checkbox and set the completed value to the opposite of the current value
+    document
+      .getElementById(`todo-${todo.id}`)
+      .addEventListener('change', () => {
+        todo.completed = !todo.completed;
 
-    console.log(todo.completed);
-    console.log(todoList);
+        console.log(todo.completed);
+        console.log(todoList);
+      });
   });
-});
+}
 
 function deleteItem(todoId) {
   todoList = todoList.filter((todo) => todo.id !== todoId);
@@ -59,4 +67,29 @@ function deleteItem(todoId) {
   if (itemToDelete) {
     ul.removeChild(itemToDelete);
   }
+  displayTodoList();
 }
+
+document.querySelector('.add-btn').addEventListener('click', function () {
+  document.getElementById('addTodoModal').style.display = 'block';
+});
+
+document.getElementById('closeModal').addEventListener('click', function () {
+  document.getElementById('addTodoModal').style.display = 'none';
+});
+
+document.getElementById('save-btn').addEventListener('click', function () {
+  const newtask = document.getElementById('new-text').value;
+
+  const newTodo = {
+    id: todoList.length + 1,
+    task: newtask,
+    completed: false,
+  };
+
+  todoList.push(newTodo);
+  displayTodoList();
+  document.getElementById('new-text').value = '';
+});
+
+displayTodoList();
